3047e44455af0b6800a93f57547c24045e4861cc
Andaukmoan <olsenze@gmail.com>
2024-04-04 16:32:56 -0600

    Update DDX4_EdU pipeline

--- slurm/DDX4_EdU_batch.sh
+++ slurm/DDX4_EdU_batch.sh
@@ -2,39 +2,49 @@
 #SBATCH --account=paternabio-rw
 #SBATCH --partition=paternabio-shared-rw
 #SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --mem 256G
-#SBATCH --time=04:00:00
+#SBATCH --ntasks=64
+#SBATCH --mem 512G
+#SBATCH --time=06:00:00
 #SBATCH --mail-user=zach.olsen@paternabio.com
 #SBATCH --mail-type=ALL
 
-CPPIPE=/uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/v7_DDX4_EdU/DDX4_EdU_v7.3.cppipe
+CPPIPE=/uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/v7_DDX4_EdU/DDX4_EdU_v7.4.cppipe
 INIMG=/uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/images/test_stardist
 OUTXL=/uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/output/test_stardist
 NTASKS=$SLURM_CPUS_ON_NODE
 
-module load deeplearning/2022.1
+FOLDERSIZE=$(($(find $INIMG/* -maxdepth 0 -type f | wc -l)/$NTASKS+1))
+echo $FOLDERSIZE" FOLDERSIZE"
 
-python /uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/python/stardist_v2.py "$INIMG"
+for FOLDER in `seq 1 $NTASKS`;
+do
+    mkdir -p "$INIMG/$FOLDER";
+    find $INIMG/* -maxdepth 0 -type f | head -n $FOLDERSIZE | xargs -i mv "{}" "$INIMG/$FOLDER"
+done
 
-module unload deeplearning/2022.1
+for FOLDER in `seq 1 $NTASKS`; do
+    (
+        IMGDIR="$INIMG/$FOLDER"
+        srun -n 1 -c 1 --mem-per-cpu=4gb --export=ALL,SUBDIR=$IMGDIR /uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/slurm/parallel_stardist.sh
+    )  &
+done
 
+wait
 echo "Finished Segmentation"
 
-module load cellprofiler
+find $INIMG/* -maxdepth 1 -type f | xargs -i mv "{}" $INIMG
 
-LENGTH=($(ls -1 $INIMG | wc -l))
+LENGTH=($(find $INIMG/* -maxdepth 0 -type f | wc -l))
+echo $LENGTH" LENGTH"
 
 SETS=$(($LENGTH/4))
-
 TASKSIZE=$(($SETS/$NTASKS+1))
-
-echo $TASKSIZE
-
+echo $TASKSIZE" TASKSIZE"
 MYARRAY=($(awk -v SETS="$SETS" -v SIZE="$TASKSIZE" 'BEGIN{e=SIZE-1;m=SETS;for(i=1;i<=m;i+=SIZE){k=m-i;if(k<SIZE){e=k};print i, i+e}}'))
+echo "${MYARRAY[1]}"" START_ARRAY"
+echo "${MYARRAY[-1]}"" END_ARRAY"
 
-echo "${MYARRAY[1]}"
-echo "${MYARRAY[-1]}"
+module load cellprofiler
 
 if [ ! -d "$OUTXL" ]; then
   mkdir "$OUTXL"
@@ -68,6 +78,6 @@ echo "$OUTDIR"
 
 module load R
 
-Rscript /uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/R/format_data.R --args "$OUTXL"
+Rscript /uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/R/format_data_2.R --args "$OUTXL"
 
 echo "Finished Analysis"
