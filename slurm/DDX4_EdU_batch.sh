e1783417fc2eecdf2a7463ff60b6ce55d874b093
Andaukmoan <olsenze@gmail.com>
2024-05-28 14:32:16 -0600

    Update DDX4_EdU_batch.sh

--- slurm/DDX4_EdU_batch.sh
+++ slurm/DDX4_EdU_batch.sh
@@ -16,15 +16,16 @@ OUTXL=/uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/output/tes
 CPPIPE=/uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/v7_DDX4_EdU/DDX4_EdU_v8.4.cppipe
 NTASKS=$SLURM_CPUS_ON_NODE
 
+#Split images into folders that can each be analyzed separately based on the number of available tasks.
 FOLDERSIZE=$(($(find $INIMG/* -maxdepth 0 -type f | wc -l)/$NTASKS+1))
 echo $FOLDERSIZE" FOLDERSIZE"
-
 for FOLDER in `seq 1 $NTASKS`;
 do
     mkdir -p "$INIMG/$FOLDER";
     find $INIMG/* -maxdepth 0 -type f | head -n $FOLDERSIZE | xargs -i mv "{}" "$INIMG/$FOLDER"
 done
 
+#Create an independent run for each subfolder and process with stardist to get around python's global interpreter lock.
 for FOLDER in `seq 1 $NTASKS`; do
     (
         IMGDIR="$INIMG/$FOLDER"
@@ -35,12 +36,15 @@ done
 wait
 echo "Finished Segmentation"
 
+#Move all images from subfolders into main folder. CellProfiler sometimes gets tripped up by subfolders.
 find $INIMG/* -maxdepth 1 -type f | xargs -i mv "{}" $INIMG
 
+#Get the number of images sets ($SETS). 
 LENGTH=($(find $INIMG/* -maxdepth 0 -type f | wc -l))
 echo $LENGTH" LENGTH"
-
 SETS=$(($LENGTH/4))
+
+#Split the number of image sets into groups based on the number of tasks available. $MYARRAY will contain the start and stop position for images in each group.
 TASKSIZE=$(($SETS/$NTASKS+1))
 echo $TASKSIZE" TASKSIZE"
 MYARRAY=($(awk -v SETS="$SETS" -v SIZE="$TASKSIZE" 'BEGIN{e=SIZE-1;m=SETS;for(i=1;i<=m;i+=SIZE){k=m-i;if(k<SIZE){e=k};print i, i+e}}'))
@@ -53,6 +57,7 @@ if [ ! -d "$OUTXL" ]; then
   mkdir "$OUTXL"
 fi
 
+#Create a separate task of cellprofiler for each image group. This allows us to run up to 64 instances of cellprofiler in parrallel.
 for ((ODD=1; ODD<${#MYARRAY[@]}; ODD+=2)); do
     (
         OUTPUT="${OUTXL}/$ODD"
@@ -73,6 +78,7 @@ if [ ! -d "$OUTIMAGE" ]; then
   mkdir "$OUTIMAGE"
 fi
 
+#Move all the annotated output images to a single folder. This makes handling them easier.
 find $OUTXL -name "*.jpeg" -type f | xargs -i mv "{}" $OUTIMAGE
 
 OUTDIR="${OUTXL}/counts"
@@ -85,6 +91,7 @@ echo "$OUTDIR"
 
 module load R
 
+#Run an r script to format the data by plate and create subfolders for each experiment that can be directly uploaded to google drive and shared with everyone.
 Rscript /uufs/chpc.utah.edu/common/HIPAA/proj_paternabio/image_analysis/R/format_data_5.R --args "$OUTXL"
 
 echo "Finished Analysis"
